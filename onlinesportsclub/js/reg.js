71a3bff43d6bac5fb46a7542b6a41ac1fc933507
vedant1551 <65863068+vedant1551@users.noreply.github.com>
2020-08-10 03:17:55 +0530

    Add files via upload

---
+++ onlinesportsclub/js/reg.js
@@ -0,0 +1,25 @@
+function myFunction() {
+  var y = document.getElementById("pass");
+  if (y.type === "password") {
+    y.type = "text";
+  } else {
+    y.type = "password";
+  }
+}
+function validate()
+  {
+
+    var ps1 = document.getElementById("pass");
+    var ps2 = document.getElementById("cpass");
+    if(ps1.value != ps2.value)
+    {
+      alert("Please Enter The Same password");
+      return false;
+    }
+    else
+    {
+      alert("Registration Successfull");
+      return true;
+    }  
+}
+
